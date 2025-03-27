538f8dbcafbe53d9ca4c6a3cdec52212bcb55710
098765lauragcrespo <paginaswebifcd@gmail.com>
2025-03-27 09:40:48 +0100

    cambio javascript

---
+++ js/javascript.js
@@ -0,0 +1,36 @@
+var grupoTarjetas = ["🦄", "🍦", "🌈", "👽", "👾", "🤖", "👹", "👺"];
+
+// EJERCICIO: concatena grupoTarjetas consigo misma para duplicar
+// los valores
+var totalTarjetas = grupoTarjetas.concat(grupoTarjetas);
+
+function barajaTarjetas() {
+
+}
+
+function reparteTarjetas() {
+
+
+  totalTarjetas.forEach(function(elemento){
+
+    });
+  // EJERCICIO: recorre el array de tarjetasBarajadas y para cada
+  // elemento, crea un div (cárgalo en una variable) y declara que su
+  // innerHTML es el div con la clase "tarjeta" y en el interior
+  // del mismo, un div con la clase "tarjeta__contenido".
+  // Añade además el valor del elemento como contenido de la tarjeta
+
+  // EJERCICIO: una vez has creado la tarjeta, y todavía dentro del
+  // loop, añade la tarjeta generada como "child" del nodo mesa.
+
+  // BONUS: podrías hacer que las tarjetas solo se repartieran cuando
+  // se apriete un botón que ponga "repartir cartas"
+}
+
+function descubrir() {
+}
+
+reparteTarjetas();
+
+document.querySelectorAll(".tarjeta").forEach(function(elemento) {
+});
